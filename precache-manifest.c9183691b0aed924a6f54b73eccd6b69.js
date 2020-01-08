924b568099424f2ab4cbacd91e33cc3e974df2d7
Phil Vargas <phil.vargas@carbonfive.com>
2020-01-08 14:37:13 -0800

    Updates

---
+++ precache-manifest.c9183691b0aed924a6f54b73eccd6b69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d70ce66577af196e05cc6a5d72c0df1",
+    "url": "/tetris/index.html"
+  },
+  {
+    "revision": "015f7b46c0a412694b44",
+    "url": "/tetris/static/css/main.d1c2c983.chunk.css"
+  },
+  {
+    "revision": "a57d4ffb6155d7730e21",
+    "url": "/tetris/static/js/2.f3da1252.chunk.js"
+  },
+  {
+    "revision": "4f8842d2c5ede7d4c10b15e150425a58",
+    "url": "/tetris/static/js/2.f3da1252.chunk.js.LICENSE"
+  },
+  {
+    "revision": "015f7b46c0a412694b44",
+    "url": "/tetris/static/js/main.aa4744b6.chunk.js"
+  },
+  {
+    "revision": "f0020a043de493d7065f",
+    "url": "/tetris/static/js/runtime-main.ec5b7f50.js"
+  }
+]);
